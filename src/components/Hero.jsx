faafc433380408f53c8cff005f749b993803f53d
Ankit Bharvad <36946135+Ankit2005@users.noreply.github.com>
2023-05-28 20:37:48 +0530

    remove 3d object

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,23 +13,23 @@ import { seo, data } from "config";
 import { motion } from "framer-motion";
 import { textVariant } from "src/utils/motion";
 import AnimatedObject from "./AnimatedObject";
-import ThreeText from "./ThreeText";
+// import ThreeText from "./ThreeText";
 
 export default function Hero() {
   return (
     <Box
       as="section"
-      //  d="flex"
+      d="flex"
       alignItems="center"
       justifyContent={{ base: "space-between" }}
       flexDir="column"
       textAlign={{ base: "center", lg: "left" }}
       mb="50px"
-      //  py={{ base: 20, md: 24 }}
-      //px={[4, 6, 10, 14, 20]}
+      py={{ base: 20, md: 24 }}
+      px={[4, 6, 10, 14, 20]}
     >
       <Box
-        // d={{ lg: "flex" }}
+        d={{ lg: "flex" }}
         justifyContent={{ lg: "center" }}
         alignItems={{ lg: "center" }}
         flexDir={{ lg: "row-reverse" }}
@@ -45,7 +45,7 @@ export default function Hero() {
           pl={{ lg: 1 == 1 && "10" }}
           pr={{ lg: 1 == 0 && "10" }}
           //width="70%"
-          height={"70vh"}
+          //  height={"70vh"}
           // border={"1px"}
           // borderColor="white"
         >
@@ -62,16 +62,16 @@ export default function Hero() {
               }}
             />
           </Box>
-          {/* <NextImage
+          <NextImage
             src={Man}
             width="600"
             height="600"
             alt={"man"}
             placeholder="blur"
             blurDataURL="L8LE.{~60000_3V@ITx^00t:V?-P"
-          /> */}
-          <ThreeText />
-          {/* <Box position="absolute" top="42" right={0}>
+          />
+          {/* <ThreeText /> */}
+          <Box position="absolute" top="42" right={0}>
             <AnimatedObject
               img={Block}
               animate={{
@@ -90,7 +90,7 @@ export default function Hero() {
                 y: ["0%", "-10%", "0%"],
               }}
             />
-          </Box> */}
+          </Box>
         </Box>
 
         <Box>
@@ -151,3 +151,141 @@ export default function Hero() {
     </Box>
   );
 }
+
+// import {
+//   Box,
+//   Button,
+//   Heading,
+//   Text,
+//   useColorModeValue,
+// } from "@chakra-ui/react";
+// import { NextSeo } from "next-seo";
+// import NextImage from "next/image";
+// import { Man } from "../assets/img";
+
+// import { seo, data } from "config";
+// import { motion } from "framer-motion";
+// import { textVariant } from "src/utils/motion";
+
+// const Home = () => {
+//   const color = useColorModeValue("red", "blue");
+
+//   const isOdd = (num) => num % 2;
+
+//   const title = "Home";
+//   const description = seo.description;
+
+//   return (
+//     <>
+//       <NextSeo
+//         title={title}
+//         description={description}
+//         canonical={seo.canonical}
+//         openGraph={{
+//           title,
+//           description,
+//           images: [
+//             {
+//               url: `${seo.canonical}bighead.svg`,
+//               width: "350px",
+//               height: "350px",
+//               alt: "avatar bigheads",
+//             },
+//           ],
+//         }}
+//       />
+//       <Box
+//         as="section"
+//         d="flex"
+//         alignItems="center"
+//         justifyContent={{ base: "space-between" }}
+//         flexDir="column"
+//         textAlign={{ base: "center", lg: "left" }}
+//         py="4"
+//       >
+//         <Box
+//           d={{ lg: "flex" }}
+//           justifyContent={{ lg: "center" }}
+//           alignItems={{ lg: "center" }}
+//           flexDir={{ lg: "row-reverse" }}
+//           w="100%"
+//         >
+//           <Box
+//             as={motion.div}
+//             {...textVariant("0.5")}
+//             pl={{ lg: 1 == 1 && "10" }}
+//             pr={{ lg: 1 == 0 && "10" }}
+//           >
+//             <NextImage
+//               src={Man}
+//               width="600"
+//               height="600"
+//               alt={"man"}
+//               placeholder="blur"
+//               blurDataURL="L8LE.{~60000_3V@ITx^00t:V?-P"
+//             />
+//           </Box>
+
+//           <Box>
+//             {/* <Heading as="h1">{"item.title"}</Heading> */}
+//             <Box
+//               as={motion.div}
+//               onScroll={() => {
+//                 // This will animate the heading when the user scrolls
+//                 motion.div.play();
+//               }}
+//               {...textVariant("0.5")}
+//             >
+//               <Text
+//                 as={motion.p}
+//                 {...textVariant("0.5")}
+//                 letterSpacing={9}
+//                 color="#878787"
+//               >
+//                 WE SERVE👇
+//               </Text>
+//               <Text
+//                 as={motion.p}
+//                 {...textVariant("0.7")}
+//                 color="offWhiteText"
+//                 fontSize="56"
+//                 fontWeight={700}
+//               >
+//                 Research & Strategy
+//               </Text>
+//               <Text
+//                 as={motion.p}
+//                 {...textVariant("0.9")}
+//                 className="out-line-text"
+//                 fontSize="56"
+//                 fontWeight={700}
+//               >
+//                 UI/UX Design
+//               </Text>
+//               <Text
+//                 as={motion.p}
+//                 {...textVariant("1")}
+//                 color="offWhiteText"
+//                 fontSize="56"
+//                 fontWeight={700}
+//               >
+//                 Web Development
+//               </Text>
+//               <Text
+//                 as={motion.p}
+//                 {...textVariant("1.1")}
+//                 className="out-line-text"
+//                 fontSize="56"
+//                 fontWeight={700}
+//               >
+//                 Mobile Development
+//               </Text>
+//             </Box>
+//           </Box>
+//         </Box>
+//       </Box>
+//     </>
+//   );
+// };
+
+// export default Home;
